163185001771143e4cc03fa6518957c971e53198
Ryan Grunest <ryangrunest45@gmail.com>
2019-05-04 10:52:38 -0700

    added passport and passport-local dependencies

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -2,11 +2,27 @@ var db = require('../models');
 
 module.exports = function(app) {
   // Load login page
-  app.get('/login', function(req, res) {
-  // render the page and pass in any flash data if it exists
+  app.get('/login', (req,res) => {
     res.render('login', {
       msg: 'Welcome!',
       examples: dbExamples
     });
+  });
+  
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }),
+  (req,res) => {
+    console.log('hello');
+
+    if (req.body.remember) {
+      req.session.cookie.maxAge = 1000 * 60 * 3;
+    } else {
+      req.session.cookie.expires = false;
+    }
+    res.redirect('/');
   });
 };

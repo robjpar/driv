0e64aa3f75fec75634da8156855900196276435c
Ryan Grunest <ryangrunest45@gmail.com>
2019-05-04 17:25:43 -0700

    fixed bug with creating new user

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,20 +13,17 @@ module.exports = function(app) {
     } else {
       res.redirect('/login');
     }
-    
   });
   app.get('/login', (req,res) => {
     res.render('login');
   });
-
   app.get('/new-user', (req,res) => {
     if (req.user) {
       res.render('new-user');
     } else {
       res.redirect('/login');
     }
-  })
-
+  });
   // Render 404 page for any unmatched routes
   app.get('*', function(req, res) {
     res.render('404');
